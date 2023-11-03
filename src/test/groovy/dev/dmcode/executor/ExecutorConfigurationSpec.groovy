93db584ee3a6a494cb27a6884975e1b12c6bc09b
Damian Malczyk <damian.malczyk.dev@gmail.com>
2023-11-03 18:56:09 +0100

    Executor configuration test

---
+++ src/test/groovy/dev/dmcode/executor/ExecutorConfigurationSpec.groovy
@@ -0,0 +1,39 @@
+package dev.dmcode.executor
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.time.Duration
+
+class ExecutorConfigurationSpec extends Specification {
+
+    @Unroll
+    def "Should reject null values"() {
+        when:
+        ExecutorConfiguration.createDefault()
+            .invokeMethod(METHOD, null)
+        then:
+        def thrown = thrown(NullPointerException)
+        thrown.message == "$PROPERTY must be provided"
+        where:
+        METHOD                         | PROPERTY
+        "withTaskInterval"             | "Task interval"
+        "withOnErrorPause"             | "On error pause"
+        "withThreadTerminationTimeout" | "Thread termination timeout"
+    }
+
+    @Unroll
+    def "Should reject negative durations"() {
+        when:
+        ExecutorConfiguration.createDefault()
+            .invokeMethod(METHOD, Duration.ofMillis(1).negated())
+        then:
+        def thrown = thrown(IllegalArgumentException)
+        thrown.message == "$PROPERTY must not be negative"
+        where:
+        METHOD                         | PROPERTY
+        "withTaskInterval"             | "Task interval"
+        "withOnErrorPause"             | "On error pause"
+        "withThreadTerminationTimeout" | "Thread termination timeout"
+    }
+}
