28c8f7dfa1f4d5289328c78b49be1156894cfb73
Damian Malczyk <damian.malczyk.dev@gmail.com>
2023-11-04 22:08:30 +0100

    Executor tests

--- src/test/groovy/dev/dmcode/executor/SingleExecutorSpec.groovy
+++ src/test/groovy/dev/dmcode/executor/SingleExecutorSpec.groovy
@@ -24,4 +24,67 @@ class SingleExecutorSpec extends Specification {
         started
         stopped
     }
+
+    def "Should start only once"() {
+        given:
+        def task = { TaskResult.AWAIT } as Task
+        def configuration = ExecutorConfiguration.createDefault()
+        def executor = new SingleExecutor(task, configuration)
+        expect:
+        executor.start()
+        !executor.start()
+        cleanup:
+        executor.stop()
+    }
+
+    def "Should stop only once"() {
+        given:
+        def task = { TaskResult.AWAIT } as Task
+        def configuration = ExecutorConfiguration.createDefault()
+        def executor = new SingleExecutor(task, configuration)
+        expect:
+        executor.start()
+        !executor.start()
+        cleanup:
+        executor.stop()
+    }
+
+    def "Should await after task"() {
+        given:
+        def taskLatch = new CountDownLatch(2)
+        def task = {
+            taskLatch.countDown()
+            TaskResult.AWAIT
+        } as Task
+        def configuration = ExecutorConfiguration.createDefault()
+        def executor = new SingleExecutor(task, configuration)
+        when:
+        executor.start()
+        then:
+        !taskLatch.await(250, TimeUnit.MILLISECONDS)
+        cleanup:
+        executor.stop()
+    }
+
+    def "Should wakeup after task"() {
+        given:
+        def taskStartedLatch = new CountDownLatch(1)
+        def taskLatch = new CountDownLatch(2)
+        def task = {
+            taskStartedLatch.countDown()
+            taskLatch.countDown()
+            TaskResult.AWAIT
+        } as Task
+        def configuration = ExecutorConfiguration.createDefault()
+        def executor = new SingleExecutor(task, configuration)
+        when:
+        executor.start()
+        taskStartedLatch.await(5, TimeUnit.SECONDS)
+        Thread.sleep(250)
+        executor.wakeup()
+        then:
+        taskLatch.await(5, TimeUnit.SECONDS)
+        cleanup:
+        executor.stop()
+    }
 }
